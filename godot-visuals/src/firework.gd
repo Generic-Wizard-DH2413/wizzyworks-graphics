62281eabd5907debeea4eaa14b53029c9291cd01
Rej Karlander <74512132+Fetaost-code@users.noreply.github.com>
2025-09-15 23:19:35 +0200

    fire work path wip

--- godot-visuals/src/firework.gd
+++ godot-visuals/src/firework.gd
@@ -16,23 +16,19 @@ func _ready():
 
 #Constantly make fw go upwards if launching
 func _physics_process(delta):
-	if(current_state == state.LAUNCHING):
-		particle_pos.y = particle_pos.y + 1
-		path.position = particle_pos
-
+	pass
 #is called from the testing_env scene	
 #pass shape points to blast scene
 func generate_shape(points):
 	if(blast != null): #AlQ: dont happen?
 		blast.set_shape(points)
 
-#path timer starts when fw is instantiated -signal-> switch state to FIRING- start explosion particle effect
-func _on_path_timer_timeout():
-	current_state = state.FIRING
-	path.queue_free() #remove path node of this fw instance (not from the scene file (globally))
-	blast.position = particle_pos
-	blast.fire()
-
-
 func _on_blast_timer_timeout():
 	blast.queue_free() #remove blast node of this fw instance (not from the scene file)
+
+#path timer starts when fw is instantiated -signal-> switch state to FIRING- start explosion particle effect
+func _on_path_path_timeout(pos) -> void:
+	current_state = state.FIRING
+	path.queue_free() #remove path node of this fw instance (not from the scene file (globally))
+	blast.position = pos
+	blast.fire()
--- godot-visuals/src/path.gd
+++ godot-visuals/src/path.gd
@@ -1,7 +1,22 @@
 extends Node3D
+signal path_timeout(pos)
+enum path_type_enum {straight, wobbly}
+@export var path_type: path_type_enum
+@export var wobble_width = 0.5
+@export var wobble_speed = 0.3
+var wobble_angle = 0
 
 func _ready():
 	var timer = get_node("PathTimer") #2s timer
 	get_node("FireLaunch").play()
 	timer.start()
+
+func _physics_process(delta: float) -> void:
+		position.y = position.y + 1
+		if path_type == path_type_enum.wobbly:
+			wobble_angle += wobble_speed
+			position.x = sin(wobble_angle)*wobble_width
 	
+
+func _on_path_timer_timeout() -> void:
+	path_timeout.emit(position)

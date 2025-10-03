6bf87545b388c8c97c439bb7939e7684c7a794ae
Alvar Trojenborg <trojenborg.alvar@gmail.com>
2025-10-03 18:02:20 +0200

    tweaked light params little bit. Plz feel free to play around w these when u create custom fw

--- godot-visuals/scenes/burst_light_3d.gd
+++ godot-visuals/scenes/burst_light_3d.gd
@@ -1,12 +1,12 @@
 extends Node3D
 
 #const DEFAULT_CULL_DISTANCE := 140.0
-@export var col_white_boost: float = 1.0
+@export var col_white_boost: float = 1.7
 @export var peak_energy: float = 10.0
-@export var incr_time: float = 10.32
-@export var decr_time: float = 1.32
-@export var range_start: float = 3.0
-@export var range_end: float = 18.0
+@export var incr_time: float = 0.32
+@export var decr_time: float = 3.32
+@export var range_start: float = 0.1
+@export var range_mid: float = 45.0
 
 func spawn_burst_light(
 	pos: Vector3,
@@ -15,8 +15,8 @@ func spawn_burst_light(
 	incr_time: float = incr_time,
 	decr_time: float = decr_time,
 	range_start: float = range_start,
-	range_end: float = range_end,
-	cast_shadows: bool = false, #should be true for hero lights
+	range_mid: float = range_mid,
+	cast_shadows: bool = true, #should be true for hero lights
 	#cull_distance: float = DEFAULT_CULL_DISTANCE
 ) -> void:
 	#var cam := get_viewport().get_camera_3d()
@@ -44,11 +44,11 @@ func spawn_burst_light(
 	var t2 := create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 
 	t1.tween_property(L, "light_energy", 0.0, (incr_time+decr_time))
-	t2.tween_property(L, "omni_range", range_end, incr_time)
+	t2.tween_property(L, "omni_range", range_mid, incr_time)
 	t2.finished.connect(func ():
 		t2.tween_property(L, "omni_range", 0.0, decr_time)
 	)
-	#t.parallel().tween_property(L, "omni_range", range_end, incr_time)
+	#t.parallel().tween_property(L, "omni_range", range_mid, incr_time)
 	t1.finished.connect(func ():
 		if is_instance_valid(L):
 			L.queue_free()

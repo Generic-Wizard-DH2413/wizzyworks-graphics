0f147ba6dc8d8ff2160e63126fdbb5f0f85138ea
Alvar Trojenborg <trojenborg.alvar@gmail.com>
2025-10-24 17:49:53 +0200

    (AoS) Amount of Shit to Pistil

--- godot-visuals/scripts/pistil_blast.gd
+++ godot-visuals/scripts/pistil_blast.gd
@@ -5,7 +5,7 @@ var middle_particles
 
 var timer
 var audio_player
-var sphere_number = 1
+var num_outer_sphere_particles = 40
 
 var firework_data = {}
 
@@ -16,12 +16,18 @@ func set_parameters(firework_data):
 	middle_particles = get_node("OuterSphereParticles2")
 	audio_player=get_node("AudioPlayer")
 	timer = get_node("CrackleTimer")
-	sphere_number = int(firework_data.get("outer_layer_specialfx", 0.5) * 9) + 1
-	
-	#set_outer_blast_data("sphere")
+	#amount of shit increases the num of outer layer particles and their Sphere Force
+	firework_data.set("outer_layer_specialfx", 1.0) #good for amount of shot testing!
+
+	num_outer_sphere_particles = int(firework_data.get("outer_layer_specialfx", 0.5) * 300) + 3 #300 is max amount 3 is min
+	outer_particles.process_material.set_shader_parameter("sphere_force", (firework_data.get("outer_layer_specialfx", 0.5) * 6) + 1) #7 is max force and 1 is min
+	$OuterSphereParticles3.amount= num_outer_sphere_particles
+	#print(outer_particles.process_material.get_shader_parameter("sphere_force"))
+	#print(firework_data.get("outer_layer_specialfx", 0.5))
+	#set_outer_blast_data("sphere") #Artifact
 	randomize()
-	#set_color()
-	set_rand_color()
+	set_color()
+	#set_rand_color()
 func _ready():
 	pass
 
@@ -60,7 +66,7 @@ func fire():
 	outer_particles.emitting = true
 	inner_particles.emitting = true
 	middle_particles.emitting = true
-	timer.start()
+	timer.start() #crackle sfx timer
 	
 func _on_crackle_timer_timeout() -> void:
 	audio_player.play_random_part()
@@ -68,27 +74,6 @@ func _on_crackle_timer_timeout() -> void:
 	# Replace with function body.
 	
 
-func spawn_rings(count):
-	var base_axis = Vector3(1, randf_range(-1,1), 1).normalized()
-	var base_angle = randf_range(0.0, 360.0)
-	inner_particles.process_material.set_shader_parameter("ring_axis", base_axis)
-	inner_particles.process_material.set_shader_parameter("ring_angle_deg", base_angle)
-	inner_particles.restart()
-	inner_particles.emitting = true
-	for i in range(count - 1):
-		var r = inner_particles.duplicate()
-		
-		var mat = r.process_material.duplicate(true)
-		r.process_material = mat
-		# random axis and angle for each ring
-		var axis = Vector3(1, randf_range(-1,1), 1).normalized()
-		var angle = randf_range(0.0, 360.0)
-		mat.set_shader_parameter("ring_axis", axis)
-		mat.set_shader_parameter("ring_angle_deg", angle)
-
-		add_child(r)
-		r.emitting = true
-		r.restart()
 		
 func set_outer_blast_data(type):
 	match(type):
@@ -116,8 +101,41 @@ func set_outer_blast_data(type):
 			
 			inner_particles.process_material.set_shader_parameter("sphere",false);
 			inner_particles.process_material.set_shader_parameter("sphere_force", 0.5)
+		
+		#ok fine I'll also make one
+		"pistil":
+				outer_particles.process_material.set_shader_parameter("sphere",true); #idk what this does. I think nothing
+				outer_particles.process_material.set_shader_parameter("sphere_force", 4.0)
+				outer_particles.process_material.set_shader_parameter("air_resistance", 2.1)
+				outer_particles.process_material.set_shader_parameter("gravity", Vector3(0,1.3,0))
+				outer_particles.process_material.set_shader_parameter("life_time", 5.5)
+				outer_particles.process_material.set_shader_parameter("start_damp", 0.9)
+				outer_particles.process_material.set_shader_parameter("damper", 1.0)
+				outer_particles.process_material.set_shader_parameter("glow_start", 300.0)
+				outer_particles.process_material.set_shader_parameter("alpha_falloff", 2.0)
+				
+				middle_particles.process_material.set_shader_parameter("sphere",true); #idk what this does. I think nothing
+				middle_particles.process_material.set_shader_parameter("sphere_force", 2.0)
+				middle_particles.process_material.set_shader_parameter("air_resistance", 2.1)
+				middle_particles.process_material.set_shader_parameter("gravity", Vector3(0,0.3,0))
+				middle_particles.process_material.set_shader_parameter("life_time", 4.0)
+				middle_particles.process_material.set_shader_parameter("start_damp", 0.5)
+				middle_particles.process_material.set_shader_parameter("damper", 2.5)
+				middle_particles.process_material.set_shader_parameter("glow_start", 300.0)
+				middle_particles.process_material.set_shader_parameter("alpha_falloff", 2.0)
+				
+				inner_particles.process_material.set_shader_parameter("sphere",true); #idk what this does. I think nothing
+				inner_particles.process_material.set_shader_parameter("sphere_force", 1.0)
+				inner_particles.process_material.set_shader_parameter("air_resistance", 1.8)
+				inner_particles.process_material.set_shader_parameter("gravity", Vector3(0,0,0))
+				inner_particles.process_material.set_shader_parameter("life_time", 5.0)
+				inner_particles.process_material.set_shader_parameter("glow_start", 300.0)
+				inner_particles.process_material.set_shader_parameter("alpha_falloff", 1.0)
+				inner_particles.process_material.set_shader_parameter("lifetime_randomness", 1.0)
 
+				
 
+				
 
 
 	

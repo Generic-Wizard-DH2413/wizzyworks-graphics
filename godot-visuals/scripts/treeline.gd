6b52fbd8ce376a86acddfcaa30c7c6893377bee7
Rej Karlander <74512132+Fetaost-code@users.noreply.github.com>
2025-10-30 10:48:05 +0100

    update script

--- godot-visuals/scripts/treeline.gd
+++ godot-visuals/scripts/treeline.gd
@@ -8,6 +8,7 @@ var speed = 0.1
 func _ready():
 	position.y = start_y
 	position.z = 320.0
+	position.x = 0.0
 
 func _physics_process(delta):
 	if(going_up):
@@ -17,10 +18,9 @@ func _physics_process(delta):
 	if(going_down):
 		position.y -= speed * delta * 60
 		if(position.y <= start_y):
-			going_up = false
-		
+			going_down = false
+			queue_free()
 
 func go_down():
 	going_down = true
-	pass
 	
